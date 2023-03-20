7a473db94860290b7152a974e74daea77e9047f3
chunkie <gsc0518@outlook.com>
2023-03-20 19:20:54 +1100

    20 Mar 2023

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LoginPage from "./pages/LoginPage"
 import StartPage from "./pages/StartPage"
 import ProfilePage from "./pages/ProfilePage"
 import NotFoundPage from "./pages/NotFoundPage"
-import { Routes, Route, useNavigate, useLocation, Link, useRoutes } from "react-router-dom"
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom"
 import { useEffect } from "react";
 import { isAuthenticated } from "./components/Login/auth";
 
@@ -21,7 +21,7 @@ function App() {
   }, [navigate])
 
   return (<>
-    <MenuBar />
+    {!isAuthenticated &&<MenuBar />}
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/match" element={<MatchPage />}>
--- src/components/Main/Loading.js
+++ src/components/Main/Loading.js
@@ -5,8 +5,8 @@ export default function Loading() {
     return (<Fragment>
         <div className="loading-container">
             <div className="loading-spinner" />
+            <h1>Matching with other pets...</h1>
         </div>
-        <h1>Matching with other pets...</h1>
     </Fragment>
     )
 }
