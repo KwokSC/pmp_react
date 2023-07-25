7da69a44c6bab0fbb47b5a141bd72593c9a6a285
chunkie <gsc0518@outlook.com>
2023-07-25 23:23:14 +1000

    25 Jul 2023

--- src/components/Main/MenuBar.js
+++ src/components/Main/MenuBar.js
@@ -1,10 +1,11 @@
 
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { removeAuthToken } from "../Login/auth";
+import { useState } from "react";
 
 export default function MenuBar() {
 
-    const navigate = useNavigate()
+    const[userId, setUserId] = useState("");
 
     function handleProfile(){
         
@@ -20,15 +21,14 @@ export default function MenuBar() {
 
     function handleLogout(){
         removeAuthToken()
-        navigate("/")
     }
 
     return (
         <div className="menu-bar">
-            <button className="profile-button" onClick={handleProfile}>Profile</button>
-            <button className="setting-button" onClick={handleSetting}>Settings</button>
-            <button className="chat-button" onClick={handleChat}>Chat</button>
-            <button className="logout-button" onClick={handleLogout}>Logout</button>
+            <Link to={"/profile/"+userId} className="button" onClick={handleProfile}>Profile</Link>
+            <Link to="/setting" className="button" onClick={handleSetting}>Settings</Link>
+            <Link to="/chat" className="button" onClick={handleChat}>Chat</Link>
+            <Link to="/" className="button" onClick={handleLogout}>Logout</Link>
         </div>
     )
 }
